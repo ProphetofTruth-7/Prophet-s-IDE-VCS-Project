7913d101d0e30b16b5f8e984380c4951e4f26a27
proph <proph@DAVIS_PC>
2025-09-15 20:53:50 -0400

    Add project files.

---
+++ Prophet's IDE-VCS Project/Prophet's IDE-VCS Project.cpp
@@ -0,0 +1,14 @@
+// Prophet's IDE-VCS Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    char first, middle, last;
+    first = 'M';
+    middle = 'R';
+    last = 'B';
+    cout << first << middle << last << endl;
+    return 8;
+
+}
