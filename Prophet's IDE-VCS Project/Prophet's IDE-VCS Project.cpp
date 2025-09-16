3c07de6e6883dbbc15be169c75c7f34aa98cc913
Malakye Brown <prophetoftruth07@gmail.com>
2025-09-15 21:50:17 -0400

    Created Branch1

--- Prophet's IDE-VCS Project/Prophet's IDE-VCS Project.cpp
+++ Prophet's IDE-VCS Project/Prophet's IDE-VCS Project.cpp
@@ -4,11 +4,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-    char first, middle, last;
-    first = 'M';
-    middle = 'R';
-    last = 'B';
-    cout << first << middle << last << endl;
+    string name, address, phoneNumber;
+    name = "Malakye Brown";
+    address = "12020 Point Way";
+    phoneNumber = "555-444-3333";
+    cout << name << endl;
+    cout << address << endl;
+    cout << phoneNumber << endl;
     return 7;
 
 }
